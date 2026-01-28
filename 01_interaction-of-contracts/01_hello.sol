a2f841aed84e021bf0e7c3002ff771779da609de
idforabhilaptop-png <idforabhilaptop@gmail.com>
2026-01-28 21:54:43 +0530

    Interaction of contracts

---
+++ 01_interaction-of-contracts/01_hello.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./IsodaPrice.sol";
+
+/**
+ * @title VendingMachine
+ * @dev A simple contract to simulate a vending machine that sells "Digital Sodas".
+ */
+contract VendingMachine {
+    // The address of the person who owns the machine.
+    address public owner;
+
+    // The number of sodas currently in the machine.
+    uint public sodaInventory;
+
+    IsodaPrice public sodaAddress;
+
+    // struct customer{
+    //     uint sodaBought;
+    //     uint lastPurchased;
+    //     bool vip ;
+    // }
+    // mapping (address => customer) public sodapurchased ;
+
+    // Set the owner and initial inventory when the contract is deployed.
+    constructor(address _sodaAddress) {
+        owner = msg.sender;
+        sodaInventory = 100; // Start with 100 sodas.
+        sodaAddress = IsodaPrice(_sodaAddress);
+    }
+
+    // A modifier to restrict certain functions to the owner only.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    /**
+     * @dev Allows anyone to buy a soda by sending exactly 0.01 ETH.
+     */
+    function buySoda() public payable {
+        // Check that there are sodas in stock.
+        require(sodaInventory > 0, "Sorry, out of stock!");
+
+        // Check that the user paid the exact price.
+        require(
+            msg.value == sodaAddress.getPrice(),
+            "You must pay exactly 0.01 ETH per soda."
+        );
+          
+        //   customer storage c1 = sodapurchased[msg.sender];
+        //   c1.sodaBought ++ ;
+
+        //   if(c1.sodaBought >= 5){
+        //     c1.vip = true ;
+        //     c1.lastPurchased = block.timestamp;
+        //   }
+
+
+        // If checks pass, decrease the inventory.
+        sodaInventory = sodaInventory - 1;
+    }
+
+    /**
+     * @dev Allows the owner to see the current Ether balance of the machine.
+     */
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    /**
+     * @dev Allows the owner to restock the machine.
+     * @param _amount The number of sodas to add to the inventory.
+     */
+    function restock(uint _amount) public onlyOwner {
+        sodaInventory = sodaInventory + _amount;
+    }
+
+    /**
+     * @dev Allows the owner to withdraw all collected profits.
+     */
+    function withdrawProfits() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No profits to withdraw.");
+
+        (bool success, ) = payable(owner).call{value: balance}("");
+        require(success, "ETH transfer failed");
+    }
+}
---
+++ 01_interaction-of-contracts/SodaPrice.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "./IsodaPrice.sol";
+
+contract SodaPrice is IsodaPrice{
+    address public owner;
+    uint public price;
+
+    constructor(){
+        owner = msg.sender;
+        price = 1 ether;
+    }
+    function getPrice() external view returns(uint){
+        return price;
+    }
+
+    function setPrice(uint _price)public {
+        require(msg.sender == owner , "Only owner can set price");
+        price = _price;
+    
+    }
+}
